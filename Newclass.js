46c20ffa5b0195e79fd5f96f56a121fdf86bd685
ggitHub-312 <74096103+ggitHub-312@users.noreply.github.com>
2021-02-12 17:23:34 +0530

    Add files via upload

---
+++ Newclass.js
@@ -0,0 +1,33 @@
+class Newclass{
+
+    constructor(b1,b2,d1,d2){
+        this.dx = d1
+        this.dy = d2
+             var options ={
+            bodyA:b1,
+            bodyB:b2,
+            pointB : {x:this.dx,y:this.dy}
+        }
+        
+        this.link=Matter.Constraint.create(options)
+
+        World.add(world,this.link)
+
+    }
+    display(){
+
+
+        var p1 = this.link.bodyA.position
+        var p2 = this.link.bodyB.position
+
+        push()
+
+        line(Bob.x,p1.y,p2.x+this.dx,p2.y + this.dy)
+        pop()
+
+       
+       
+    
+}
+
+}
